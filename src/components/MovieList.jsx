dc22bf1c009dc564f01e9e7111525209da7c0353
Horlaryimika <Busariolayinka036@gmail.com>
2025-02-04 00:41:52 +0100

    movieCard edit

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import SeeMore from "./navLinks/SeeMore";
+import Genre from "./button/Genre";
+import MovieCard from "./MovieCard";
 
 const MovieList = () => {
+  const [getMovies, setGetMovies] = useState([]);
+  const API_URL_ONE =
+    "https://api.themoviedb.org/3/discover/movie?api_key=bcc26b7e142a51f09bcf0a149964e33b";
+
+  useEffect(() => {
+    fetch(API_URL_ONE) //for hero section
+      .then((res) => res.json())
+      .then((data) => {
+        setGetMovies(data.results);
+      });
+  }, []);
+
   return (
-    <section>MovieList</section>
-  )
-}
+    <section>
+      <div>
+        <h1>Movies</h1>
+        <SeeMore />
+      </div>
+      <div>
+        <Genre />
+      </div>
+      <div>
+        {getMovies &&
+          getMovies.map((movie, index) => {
+            return <MovieCard key={movie.id} {...movie} />;
+          })}
+      </div>
+    </section>
+  );
+};
 
 export default MovieList;
