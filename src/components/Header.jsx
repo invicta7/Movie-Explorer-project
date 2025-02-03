4df1e5d31fd6224cd7975bf9b622f060ebc34724
Ornerd <ukoorbytal@gmail.com>
2025-02-03 16:18:11 +0100

    small css fix on mobile view

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -45,7 +45,7 @@ const Header = () => {
               <div className='w-full md:w-[85%] md:max-w-[1232px] pb-6 px-4 lg:px-0 flex items-center lg:items-end flex-col-reverse lg:flex-row lg:justify-between relative'>
 
                 {/* movie text area */}
-                <div className='w-full lg:w-2/5 pb-6'>
+                <div className='w-full h-80 lg:h-auto lg:w-2/5 pb-6'>
                   <h1 className='text-5xl font-bold mb-4'>{featuredMovie.title}</h1>
                   <p className='text-left'>{featuredMovie.overview}</p>
                   <span className='flex gap-4 items-center justify-left pt-4'>
@@ -61,7 +61,7 @@ const Header = () => {
                 {/* indicator area */}
                 <div className='w-full lg:w-fit px-8 z-40 flex items-center justify-start mb-8 lg:mb-0'>
                   {featuredMovies && featuredMovies.map((featuredMovie, index)=> {
-                    return <button key={featuredMovie.id} onClick={()=>setCurrentIndex(index)} className={`cursor-pointer flex items-start justify-center w-25 h-25 md:w-35 md:h-35 overflow-hidden rounded-2xl -mx-2 relative border-[1px] border-[#228de557] ${currentIndex==index? "z-50 w-40 h-40 md:w-50 md:h-50": " "}`}>
+                    return <button key={featuredMovie.id} onClick={()=>setCurrentIndex(index)} className={`cursor-pointer flex items-start justify-center w-25 h-25 md:w-35 md:h-35 overflow-hidden rounded-2xl -mx-2 relative border-[1px] border-[#228de557] nth-3:z-[20] ${currentIndex==index? "z-50 w-40 h-40 md:w-50 md:h-50": " "}`}>
                     <img src={API_URL_IMG+featuredMovie.poster_path} alt="movie-poster"/>
                     <span className={`w-full h-full absolute pointer-events-none ${currentIndex==index? " ": "bg-[#030a1b86]"}`}></span>
                   </button>
