5a722ecdc0ba8a9a54be1d88e30ed4a5b86b1667
invicta7 <vick4dermatology@gmail.com>
2025-02-05 22:02:08 +0100

    Left and Right arrow buttons idea

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,13 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer>
-      <div className='w-full h-full justify-between flex items-center  absolute'>
-        <h1>Footer</h1>
+      <div className='w-full h-full justify-between flex items-center relative py-40'>
+        
+        <div className='w-full h-[464px] border-solid border-[1px] border-[#EC5BAA] absolute flex items-center justify-center mt-50'>
+        <h1 className='text-[48px] font-bold flex m-auto'>Studios</h1>
+        
+          <img src=''></img>
+        </div>
 
       </div>
     </footer>
