e39df2c2f21804efca848c0878d65557caffa77d
Horlaryimika <Busariolayinka036@gmail.com>
2025-02-06 23:57:48 +0100

    saved favorite

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,15 +3,18 @@ import SeeMore from "./navLinks/SeeMore";
 import Genre from "./button/Genre";
 import MovieCard from "./MovieCard";
 import { motion } from "framer-motion";
+import FavoriteList from "./FavoriteList"
 
 const MovieList = () => {
   const [getMovies, setGetMovies] = useState([]);
   const [getGenres, setGetGenres] = useState([]);
+  const [savedFavorite, setSavedFavorite] = useState([]);
 
   const API_URL_ONE =
     "https://api.themoviedb.org/3/discover/movie?api_key=bcc26b7e142a51f09bcf0a149964e33b";
 
-  const GENRE_API = "https://api.themoviedb.org/3/genre/movie/list?api_key=bcc26b7e142a51f09bcf0a149964e33b";
+  const GENRE_API =
+    "https://api.themoviedb.org/3/genre/movie/list?api_key=bcc26b7e142a51f09bcf0a149964e33b";
 
   useEffect(() => {
     fetch(API_URL_ONE) //for hero section
@@ -21,41 +24,38 @@ const MovieList = () => {
       });
   }, []);
 
-  useEffect(()=> {
+  useEffect(() => {
     fetch(GENRE_API)
       .then((result) => result.json())
       .then((data) => {
         setGetGenres(data.genres);
-        console.log(getGenres)
+        console.log(getGenres);
       });
-  }, [])
+  }, []);
 
   return (
     <section className="max-w-[1232px] mx-auto px-4">
+      <FavoriteList savedFavorite={savedFavorite}/>
       <div className="text-2xl flex justify-between mb-8">
         <h1>Movies</h1>
         <SeeMore />
       </div>
       <div className="flex justify-start w-full mb-12 overflow-x-auto">
-        {
-          getGenres && getGenres.map((genre)=> {
-            return <Genre key={genre.id} genreName={genre.name}/>
-          })
-        }
-      
+        {getGenres &&
+          getGenres.map((genre) => {
+            return <Genre key={genre.id} genreName={genre.name} />;
+          })}
       </div>
       <div className="w-full flex items-start justify-center gap-8 scroll-smooth scrollbar-hide flex-wrap relative">
-        {getMovies && getMovies.map((movie, index ) =>  {
+        {getMovies &&
+          getMovies.map((movie, index) => {
             return (
-            <motion.div 
-              initial={{scale: 1}}
-              whileHover={{scale: 1.1}}><MovieCard key={movie.id} {...movie} getGenres={getGenres} /> 
-            </motion.div>
-            )
+              <motion.div initial={{ scale: 1 }} whileHover={{ scale: 1.1 }}>
+                <MovieCard key={movie.id} {...movie} getGenres={getGenres} />
+              </motion.div>
+            );
           })}
-     </div>
-
-
+      </div>
     </section>
   );
 };
